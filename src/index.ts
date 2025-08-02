94aebd34002f5966b90cedbacd38d377e24f36f5
dingzhenznen <1324652616@qq.com>
2025-08-02 15:29:34 +0800

    init proxy

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import { isProd } from './constants';
 import { initS3Server } from './s3/config';
 import { connectSignoz } from './utils/signoz';
 import { initModels } from '@model/init';
+// import setupProxy from './utils/proxy';
+// setupProxy()
 
 const app = express().use(
   express.json(),
