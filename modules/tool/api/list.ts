23a3b3e618913da7e39a0565f3c5590526ad3fac
dingzhenznen <1324652616@qq.com>
2025-08-06 09:43:35 +0800

    fix:update list

--- modules/tool/api/list.ts
+++ modules/tool/api/list.ts
@@ -3,34 +3,58 @@ import { contract } from '@/contract';
 import { tools } from '@tool/constants';
 import { formatToolList } from '@tool/utils/tool';
 
+import axios from 'axios';
+
 import https from 'https';
 
 import http from 'http';
 
+import { setGlobalDispatcher, ProxyAgent }  from "undici";
+const httpDispatcher = new ProxyAgent( "http://127.0.0.1:7897");
+setGlobalDispatcher(httpDispatcher);
+
+
 export const getToolsHandler = s.route(contract.tool.list, async () => {
-  const req = https.get('https://httpbin.org/ip', (res: any) => {
-    console.log('statusCode:', 'ssdsdsd');
-    res.on('data', (chunk: any) => {
-      console.log('gteese', chunk.toString());
-    });
-    req.on('error', (err: any) => {});
 
-    res.on('end', () => {
-      console.log('dsdsd');
-    });
-  });
 
-  const req2 = http.get('http://49.233.46.121:30840/login', (res: any) => {
-    console.log('statusCode:', 'ssdsdsd');
-    res.on('data', (chunk: any) => {
-      console.log('gteese', chunk.toString());
-    });
-    req.on('error', (err: any) => {});
+  // fetch("https://httpbin.org/ip")
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     console.log("fetch IP:", res);
+  //   })
+  //   .catch((err) => console.log("请求失败：", err));
 
-    res.on('end', () => {
-      console.log('dsdsd');
-    });
-  });
+
+  // axios.get('http://httpbin.org/ip')
+  // .then(response => {
+  //   console.log('axios IP:', response.data.origin);
+  // })
+  // .catch(error => {
+  //   console.error('请求失败:', error.message);
+  // });
+
+  // const req = https.get('https://httpbin.org/ip', (res: any) => {
+  //   res.on('data', (chunk: any) => {
+  //     console.log('https', chunk.toString());
+  //   });
+  //   req.on('error', (err: any) => {});
+
+  //   res.on('end', () => {
+  //     console.log('done');
+  //   });
+  // });
+
+  // const req2 = http.get('http://49.233.46.121:30840/login', (res: any) => {
+  //   console.log('statusCode:', 'ssdsdsd');
+  //   res.on('data', (chunk: any) => {
+  //     console.log('gteese', chunk.toString());
+  //   });
+  //   req.on('error', (err: any) => {});
+
+  //   res.on('end', () => {
+  //     console.log('dsdsd');
+  //   });
+  // });
 
   req.end();
 
