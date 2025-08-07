2af82171559cae0fc90cb592aa150c85a99bc6c6
dingzhenznen <1324652616@qq.com>
2025-08-07 14:32:24 +0800

    fix:update

--- modules/tool/utils/initProxy.ts
+++
@@ -1,8 +0,0 @@
-
-//console.log("init fetch Proxy");
-import {ProxyAgent,setGlobalDispatcher} from "undici";
-if(process.env.HTTP_PROXY){
-  const httpDispatcher = new ProxyAgent(process.env.HTTP_PROXY as string);
-  setGlobalDispatcher(httpDispatcher);
-}
-
